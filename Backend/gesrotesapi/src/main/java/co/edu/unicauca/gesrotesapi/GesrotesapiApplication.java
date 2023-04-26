79fcefeabe8e4cd4739f0c693eb8df6e25c9da93
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-04-25 23:06:28 -0500

    frontend y backend

---
+++ Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/GesrotesapiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.gesrotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesrotesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesrotesapiApplication.class, args);
+	}
+
+}
---
+++ Backend/gesrotesapi/src/test/java/co/edu/unicauca/gesrotesapi/GesrotesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.gesrotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesrotesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
