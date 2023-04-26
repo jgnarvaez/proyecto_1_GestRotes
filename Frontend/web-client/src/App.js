b34ab75698e862ff110c4d3bb7ef20fd9364fa48
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-04-25 23:27:11 -0500

    prueba

--- Frontend/web-client/src/App.js
+++ Frontend/web-client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React new project
+          Learn React new project 1
         </a>
       </header>
     </div>
