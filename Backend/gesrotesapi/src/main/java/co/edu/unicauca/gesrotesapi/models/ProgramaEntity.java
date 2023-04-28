e40a13c10569f310e223a8cdcb64cb5014d505d7
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-04-27 20:53:45 -0500

    web config para acpetar solicitudes

--- Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/models/ProgramaEntity.java
+++ Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/models/ProgramaEntity.java
@@ -1,7 +1,5 @@
 package co.edu.unicauca.gesrotesapi.models;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
--- Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/services/services/ServiceAsignaturaImpl.java
+++ Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/services/services/ServiceAsignaturaImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
