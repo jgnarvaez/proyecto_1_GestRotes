7523201bc21b1e8b78a8fc62ef587b285152ec51
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-04-27 21:13:11 -0500

    CORS fixed

--- Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/WebConfig.java
+++ Backend/gesrotesapi/src/main/java/co/edu/unicauca/gesrotesapi/WebConfig.java
@@ -9,11 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        .allowedOrigins("")
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("")
         .allowCredentials(true)
         .maxAge(3600);
     }
+
 }
