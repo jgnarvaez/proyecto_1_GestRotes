7af0d9db8b3017e39fdcc474f65b3375157a3362
ArnoldCatamuscay <ascatamuscay@unicauca.edu.co>
2023-04-29 23:25:15 -0500

    cambios a la bd

--- Modelos de base de datos/Workbench/inserts.sql
+++ Modelos de base de datos/Workbench/inserts.sql
@@ -9,25 +9,25 @@ VALUES
 -- coordinador
 INSERT INTO mydb.tbl_coordinador (coo_nombres, coo_apellidos, coo_correo, coo_clave, coo_rol, coo_foto_perfil)
 VALUES
-('Jorge Ivan', 'Solano', 'jsolano@unicauca.edu.co', '1234', 'Coordinador de asignatura', 'url foto');
+('Jorge Ivan', 'Solano', 'jsolano', '1234', 'Coordinador_de_asignatura', 'url foto');
 
 -- coordinador de asignatura
 INSERT INTO mydb.tbl_coordinador_asignatura (coo_id) VALUE (1);
 
 -- asignatura
-INSERT INTO mydb.tbl_asignatura (asig_nombre, coo_id, prog_id)
+INSERT INTO mydb.tbl_asignatura (asig_nombre)
 VALUES 
-('Cuidado de Enfermería y Salud Pública', 1, 1),
-('Psicología General', 1, 1),
-('Cuidado de Enfermería en salud comunitaria y familiar', 1, 1),
-('Morfología', 1, 1),
-('Microbiología', 1, 1),
-('Cuidado a las personas con Procesos infecciosos', 1, 1),
-('Farmacología', 1, 1),
-('Cuidado de Enfermería en Salud Mental', 1, 1),
-('Cuidado de Enfermería al Adulto y al Anciano I', 1, 1),
-('Enfermería en salud nutricional', 1, 1),
-('Cuidado al Niño y al Adolescente', 1, 1);
+('Cuidado de Enfermería y Salud Pública'),
+('Psicología General'),
+('Cuidado de Enfermería en salud comunitaria y familiar'),
+('Morfología'),
+('Microbiología'),
+('Cuidado a las personas con Procesos infecciosos'),
+('Farmacología'),
+('Cuidado de Enfermería en Salud Mental'),
+('Cuidado de Enfermería al Adulto y al Anciano I'),
+('Enfermería en salud nutricional'),
+('Cuidado al Niño y al Adolescente');
 
 -- escenario_practica
 INSERT INTO mydb.tbl_escenario_practica (esc_nombre)
@@ -58,11 +58,78 @@ VALUES
 ('Mañana y tarde','11:30','21:30');
 
 -- persona universitaria
-INSERT INTO mydb.tbl_persona_universitaria (pu_nombres, pu_apellidos, pu_identificacion, pu_correo, pu_foto_perfil, prog_id)
+INSERT INTO mydb.tbl_persona_universitaria (pu_nombres, pu_apellidos, pu_identificacion, pu_usuario, pu_foto_perfil)
 VALUES
-('Cristian','Gomez Santos',10611,'cgsantos@unicauca.edu.co','url foto',1);
+('Cristian','Gomez Santos',106813022001,'cgsantos','url foto'),
+('Cristobal','Colon Lopez',106813022002,'cclopez','url foto'),
+('Maria','Paz Bolaños',106813022003,'mpbolanos','url foto'),
+('Juan Camilo','Sanchez',106813022004,'jcsacnchez','url foto'),
+('Hector Estenam','Coral',106813022005,'hecoral','url foto'),
+('Juan Sebastian','Perez',106813022006,'jsperez','url foto'),
+('Paula Andrea','Velez',106813022007,'pavelez','url foto'),
+('Natalia','Ortiz Bolaños',106813022008,'nbolanos','url foto');
 
 -- estudiante
 INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (1);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (2);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (3);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (4);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (5);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (6);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (7);
+INSERT INTO mydb.tbl_estudiante (pu_id) VALUE (8);
+
+-- matricula
+INSERT INTO mydb.tbl_matricula (prog_id, pu_id)
+VALUES
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,8);
+
+-- asignacion
+INSERT INTO mydb.tbl_asignacion (prog_id, asig_id, coo_id)
+VALUES
+(1,8,1),
+(1,9,1),
+(1,10,1);
+
+-- est_asignacion
+INSERT INTO mydb.tbl_est_asignacion (pu_id, prog_id, asig_id, coo_id)
+VALUES
+(3,1,8,1),
+(4,1,8,1),
+(5,1,8,1),
+(6,1,8,1),
+(7,1,8,1),
+(8,1,8,1);
 
 
+-- Consultas
+-- listar estudiantes
+SELECT pu.pu_id, pu.pu_nombres, pu.pu_identificacion, pu.pu_usuario
+FROM mydb.tbl_persona_universitaria pu
+INNER JOIN mydb.tbl_estudiante e ON pu.pu_id = e.pu_id
+INNER JOIN mydb.tbl_est_asignacion ea ON e.pu_id = ea.pu_id
+WHERE ea.asig_id = 8
+  AND ea.prog_id = 1
+  AND ea.coo_id = 1
+
+-- Buscar por nombre, cuales no estan registrados
+SELECT pu.pu_id, pu.pu_nombres, pu.pu_identificacion, pu.pu_correo
+FROM tbl_persona_universitaria pu
+INNER JOIN tbl_estudiante e ON pu.pu_id = e.pu_id
+WHERE pu.pu_nombres LIKE '%Cr%'
+AND e.pu_id NOT IN (
+    SELECT ea.pu_id
+    FROM tbl_est_asignacion ea
+    WHERE ea.asig_id = 8
+      AND ea.prog_id = 1
+      AND ea.coo_id = 1
+)
+
+-- eliminar todos los estudiantes 
